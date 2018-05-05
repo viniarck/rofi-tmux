d708bdba40f5bc21661003b99131380e99966ae9
Vinicius Arcanjo <viniciusarcanjov@gmail.com>
2018-05-05 11:13:46 -0300

    updated setup.py and set python-rofi version

--- setup.py
+++ setup.py
@@ -7,16 +7,16 @@ setup(
     version=__version__,
     description=desc,
     author='Vinicius Arcanjo',
-    author_email='viniciusarcanjov8@gmail.com',
+    author_email='viniciusarcanjov@gmail.com',
     keywords='rofi tmux tmuxinator i3 manage switch',
     url='http://github.com/viniciusarcanjo/rofi-tmux',
-    packages=['rft', 'bin'],
+    packages=['rft', 'rft/bin'],
     license='MIT',
-    install_requires=['python-rofi', 'libtmux', 'i3ipc', 'click'],
+    install_requires=['python-rofi==1.0.1', 'libtmux', 'i3ipc', 'click'],
     entry_points='''
         [console_scripts]
-        rft=bin.main:main
-        rofi-tmux=bin.main:main
+        rft=rft.bin.main:main
+        rofi-tmux=rft.bin.main:main
     ''',
     zip_safe=False,
 )
